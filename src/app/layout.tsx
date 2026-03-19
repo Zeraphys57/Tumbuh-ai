de26b8833b1fd64d513156d38968b45b0f0a5266
JacquellCode <bryanjacquellino5757@gmail.com>
2026-03-19 19:30:22 +0700

    feat: add facebook domain verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="facebook-domain-verification" content="en2a6nn4xuoe70jdw20u1sa7ordef6" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
